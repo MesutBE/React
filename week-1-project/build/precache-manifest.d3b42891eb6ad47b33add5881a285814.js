9b1f2427805cece40fd1bb131d0722a5e0342426
mesutBE <gelukkig.zijn.be@gmail.com>
2020-06-20 20:56:19 +0200

    challenge

---
+++ week-1-project/build/precache-manifest.d3b42891eb6ad47b33add5881a285814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051ff736491d797f68f6b389a2779cca",
+    "url": "/React/week-1-project/build/index.html"
+  },
+  {
+    "revision": "eab2181de029d235b562",
+    "url": "/React/week-1-project/build/static/js/2.5670d731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/week-1-project/build/static/js/2.5670d731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b372ca3dbbce6a3f85",
+    "url": "/React/week-1-project/build/static/js/main.785cde5a.chunk.js"
+  },
+  {
+    "revision": "75313d0aeb2d6c0a9f75",
+    "url": "/React/week-1-project/build/static/js/runtime-main.0e686d82.js"
+  }
+]);
